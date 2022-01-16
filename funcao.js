596cefbbf8d5b664c06fd3adf68d184d9bbc767d
Francildo Brito <96621544+FrancildoBrito@users.noreply.github.com>
2022-01-16 00:01:10 -0300

    Criando calculadora

---
+++ funcao.js
@@ -0,0 +1,37 @@
+function calcular() {
+    let peso = document.getElementById('txtpes')
+    let altura = document.getElementById('txtalt')
+    let res = document.getElementById('res')
+
+    let p = Number(peso.value)
+    let alt = Number(altura.value)
+    let imc = p / (alt **2)
+
+    if(imc >= 18.5 && imc <= 24.9){
+        res.innerHTML = `<p> O seu IMC é de ${imc.toFixed(2)},
+         esse valor é considerado NORMAL. Procure um médico para um resultado mais confiavél</p>`
+         res.style.background= 'green'
+         res.style.color= 'white'
+
+    }else if(imc < 30.0){
+        res.innerHTML = `<p> O seu IMC é de ${imc.toFixed(1)},
+         esse valor é considerado como SOBREPESO! Procure um médico para um resultado mais confiavél.</p>`
+         res.style.background= 'orangered'
+         res.style.color= 'white'
+
+    }else if(imc > 30) {
+        res.innerHTML = `<p> O seu IMC é de ${imc.toFixed(1)},
+        esse valor é considerado como OBESIDADE! Procure um médico para um resultado mais confiavél.</p>`
+        res.style.background= 'red'
+        res.style.color= 'white'
+      
+    } if(imc < 18.5){
+        res.innerHTML = `<p> O seu IMC é de ${imc.toFixed(1)},
+         esse valor é considerado como MAGREZA! Procure um médico para um resultado mais confiavél.</p>`
+         res.style.background= 'orangered'
+         res.style.color= 'white'
+
+        }
+        
+   
+}
